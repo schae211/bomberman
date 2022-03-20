6688ae0422642eb35b34d8250b993b6ebef1c274
schae211 <schae211@msu.edu>
2022-03-20 12:31:55 +0100

    proper feat eng for config

--- agent_code/nn_agent_v2/config.py
+++ agent_code/nn_agent_v2/config.py
@@ -30,7 +30,7 @@ configs = edict({
     # default probabilities for the actions [up, right, down, left, wait, bomb]
     "DEFAULT_PROBS": [.2, .2, .2, .2, .1, .1],
     # determines the behavior of the states_to_features function: {"channels", "standard", "channels+bomb", "channels_reduced}
-    "FEATURE_ENGINEERING": "standard",
+    "FEATURE_ENGINEERING": "channels",
     # what loss to use for nn: {mse, huber}
     "LOSS": "huber",
     # learning rate used for gradient descent in nn
